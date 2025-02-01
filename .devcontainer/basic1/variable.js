3116dd36a796e1db80efc8c1a00fc9f1dbba4e2b
Shubham Singh <158585057+ShubhamSinghDev@users.noreply.github.com>
2025-02-01 14:22:39 +0000

    varibale completed

---
+++ .devcontainer/basic1/variable.js
@@ -0,0 +1,12 @@
+const accountId=123
+let accountEmail="shubham@gmaol.com"
+var accountPass="111"
+accountCity="Jaipur"
+
+accountEmail="priyanka@gmail.com"
+accountPass="5656"
+accountCity="Bengaluru"
+
+console.log(typeof(accountCity))
+
+console.table([accountEmail,accountId,accountPass,accountCity])
